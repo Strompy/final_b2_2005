1e0771b3871a99cc6db23996c721551d5265bc33
Phillip Strom <phillip.strom@gmail.com>
2020-08-03 10:43:26 -0600

    Create model methods for number of passengers on flight

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -13,4 +13,29 @@ RSpec.describe Flight do
     it { should have_many :flight_passengers }
     it { should have_many(:passengers).through(:flight_passengers) }
   end
+  describe "instance methods" do
+    before :each do
+      @airline = Airline.create!(name: "Trans American Airlines")
+      @flight1 = @airline.flights.create!(number: "1727", date: "08/03/20", time: "10:12", departure_city: "Denver", arrival_city: "Reno")
+      @passenger1 = Passenger.create!(name: "Kareem Abdul-Jabbar", age: 32)
+      @passenger2 = Passenger.create!(name: "Elaine Dickinson", age: 25)
+      @passenger3 = Passenger.create!(name: "Ted Striker", age: 30)
+      @passenger4 = Passenger.create!(name: "Otto", age: 3)
+      @passenger5 = Passenger.create!(name: "Joey", age: 8)
+      FlightPassenger.create!(flight: @flight1, passenger: @passenger1)
+      FlightPassenger.create!(flight: @flight1, passenger: @passenger2)
+      FlightPassenger.create!(flight: @flight1, passenger: @passenger3)
+      FlightPassenger.create!(flight: @flight1, passenger: @passenger4)
+      FlightPassenger.create!(flight: @flight1, passenger: @passenger5)
+    end
+    it "counts total_passengers_count" do
+      expect(@flight1.total_passengers_count).to eq(5)
+    end
+    it "counts number_of_minors" do
+      expect(@flight1.number_of_minors).to eq(2)
+    end
+    it "counts number_of_minors" do
+      expect(@flight1.number_of_adults).to eq(3)
+    end
+  end
 end
