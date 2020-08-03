e0e8e97cce61214fe26a1a7375d9b1517579e2b7
Phillip Strom <phillip.strom@gmail.com>
2020-08-03 11:48:36 -0600

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get '/flights/:id', to: 'flights#show'
 
   resources :passengers, only: [:show]
-  patch '/passengers/:id/add_flight', to: 'flight_passengers#create'
+  post '/passengers/:id/add_flight', to: 'flight_passengers#create'
 end
