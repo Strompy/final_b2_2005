d16462f69fc9ce9910e2a8602b4039c34af5184a
Phillip Strom <phillip.strom@gmail.com>
2020-08-03 09:42:00 -0600

    Create flight_passengers

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -10,5 +10,7 @@ RSpec.describe Flight do
   end
   describe "relationships" do
     it { should belong_to :airline }
+    it { should have_many :flight_passengers }
+    it { should have_many(:passengers).through(:flight_passengers) }
   end
 end
